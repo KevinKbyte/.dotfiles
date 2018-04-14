226a4bf54ecb4ace710155863333de8ec992855a
Kevin Liang <22605103+KevinKbyte@users.noreply.github.com>
2018-04-14 14:53:21 -0400

    added cursor jumping functionality and generator.py to generate the cursor jumps
    Signed-off-by: Kevin Liang <22605103+KevinKbyte@users.noreply.github.com>

---
+++ i3/scripts/generator.py
@@ -0,0 +1,110 @@
+from string import ascii_lowercase
+from sys import argv
+from os import system
+
+if len(argv) == 6:
+    X_INCREMENTS = argv[1]
+    Y_INCREMENTS = argv[2]
+    OFFSET = argv[3]
+    X_CHUNK_SIZE = argv[4]
+    Y_CHUNK_SIZE = argv[5]
+else:
+    screen_size_filename = "/tmp/mouse-jump"
+    system("xrandr  | grep \* | cut -d' ' -f4 > " + screen_size_filename)
+
+    # screen size
+    S_SIZE_FILE = open(screen_size_filename, "r")
+    S_SIZE = str(S_SIZE_FILE.readline())
+
+    X, Y = S_SIZE.split("x")
+    X, Y = int(X), int(Y)
+
+    # X Chunk Size = 10 * (# of key combinations)
+    # # of key combinations:
+        # Key
+        # Mode_switch + Key
+    X_KEY_COMBOS = 2
+    X_CHUNK_SIZE = 10 * X_KEY_COMBOS
+
+    # Y Chunk Size = 4 * (# of key combinations)
+    # 4 is because 1-->0, q-->p, a-->;, z-->/ are four layers of y axis matrix
+    # # of key combinations:
+        # Key
+        # Mod + key
+        # Shift + key
+        # Mod + Shift + key
+    # Y Chunk Size = 4 * 3 = 16
+    Y_KEY_COMBOS = 4
+    Y_CHUNK_SIZE = 4 * Y_KEY_COMBOS
+
+    X_INCREMENTS = int(round(X / X_CHUNK_SIZE))
+    Y_INCREMENTS = int(round(Y / Y_CHUNK_SIZE))
+
+JUMPER = "/home/kevin/.config/i3/scripts/mouse_jumper.py"
+
+NUMBERS = [ ['grave'] + map(str, range(1, 10)) ] # x = 0 --> 9; y = 0 --> 2
+ALPHABET = [[ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ]] # x = 0 --> 9; y = 3 --> 5
+ALPHABET += [['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon' ]] # x = 0 --> 9; y = 6 --> 8
+ALPHABET += [[ 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash' ]] # x = 0 --> 9; y = 9 --> 11
+
+NUM_ALPHA = NUMBERS + ALPHABET
+i = 0 # iterates through the letters of NUM_ALPHA
+j = 0 # controls y start value
+k = j + Y_KEY_COMBOS
+
+mode_default = " mode \"default\";"
+mode_default = ""
+
+rm_i3bar = "; exec xdotool keydown super keyup super"
+rm_i3bar = ""
+
+
+num_alpha_x = 0 # iterates through the lists of NUM_ALPHA
+
+for y in range(0, Y_CHUNK_SIZE, Y_KEY_COMBOS):
+    for x in range(0, X_CHUNK_SIZE, X_KEY_COMBOS):
+        # exec python /home/kevin/.config/i3/scripts/mouse_jumper.py 2 0; exec xdotool keydown super keyup super
+        # bindsym $mod+3 mode "default"; exec python /home/kevin/.config/i3/scripts/mouse_jumper.py 2 1; exec xdotool keydown super keyup super
+        # bindsym $mod+shift+3 mode "default"; exec python /home/kevin/.config/i3/scripts/mouse_jumper.py 2 2; exec xdotool keydown super keyup super
+
+        print("bindsym " + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x) + " " + str(y)) + rm_i3bar
+        print("bindsym " + "$mod+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x) + " " + str(y+1)) + rm_i3bar
+        print("bindsym " + "shift+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x) + " " + str(y+2)) + rm_i3bar
+        print("bindsym " + "$mod+shift+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x) + " " + str(y+3)) + rm_i3bar
+        print("bindsym " + "Mode_switch+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x+1) + " " + str(y)) + rm_i3bar
+        print("bindsym " + "Mode_switch+$mod+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x+1) + " " + str(y+1)) + rm_i3bar
+        print("bindsym " + "Mode_switch+shift+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x+1) + " " + str(y+2)) + rm_i3bar
+        print("bindsym " + "Mode_switch+$mod+shift+" + NUM_ALPHA[num_alpha_x][i] + mode_default + " exec python " + JUMPER + " " + str(x+1) + " " + str(y+3)) + rm_i3bar
+        i+=1
+        print("\n")
+    i = 0
+    num_alpha_x += 1
+
+rm_i3bar = "; exec xdotool keydown super keyup super"
+mode_default = " mode \"default\";"
+
+mode_click = "Space"
+left_click = "j"
+right_click = "k"
+middle_click = "Space"
+click_default_mode = "Escape"
+cursor_default_mode = "Return"
+mouse_move_mode = "$mod+a"
+
+print("bindsym " + mode_click + " mode \"mode_click\"")
+print("bindsym " + cursor_default_mode + " mode \"default\"" + rm_i3bar)
+print("\n}\n")
+print("bindsym " + mouse_move_mode + " mode \"mode_mouse_move\"")
+print("\n")
+
+print("set $mode_click")
+print("mode \"mode_click\" {\n")
+# Left clicks mouse
+print("    bindsym " + left_click + mode_default + " exec xdotool click 1")
+# Right clicks mouse
+print("    bindsym " + right_click + mode_default + " exec xdotool click 3")
+# Middle mouse
+print("    bindsym " + middle_click + mode_default + " exec xdotool click 2")
+# Default Mode
+print("    bindsym " + click_default_mode + " mode \"default\"" + rm_i3bar)
+print("\n}")
---
+++ i3/scripts/mouse_jumper.py
@@ -0,0 +1,30 @@
+from os import system
+from sys import argv
+
+screen_size_filename = "/tmp/mouse-jump"
+system("xrandr  | grep \* | cut -d' ' -f4 > " + screen_size_filename)
+
+# screen size
+S_SIZE_FILE = open(screen_size_filename, "r")
+S_SIZE = str(S_SIZE_FILE.readline())
+
+X, Y = S_SIZE.split("x")
+X, Y = int(X), int(Y)
+
+X_CHUNK_SIZE = 20
+Y_CHUNK_SIZE = 16
+
+X_INCREMENTS = int(round(X / X_CHUNK_SIZE))
+Y_INCREMENTS = int(round(Y / Y_CHUNK_SIZE))
+
+OFFSET = 20
+
+X_LOCATION = str(int(argv[1]) * X_INCREMENTS + OFFSET)
+Y_LOCATION = str(int(argv[2]) * Y_INCREMENTS + OFFSET)
+print X_INCREMENTS, Y_INCREMENTS
+
+print X_LOCATION, Y_LOCATION
+
+system("xdotool mousemove " + X_LOCATION + " " + Y_LOCATION)
+
+S_SIZE_FILE.close()
