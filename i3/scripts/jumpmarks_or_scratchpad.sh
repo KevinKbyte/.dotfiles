974e75378480be764471b28d0a6122895e7a9d2d
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2018-11-22 22:51:53 -0500

    fixed the dynamic mark jumping script
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- i3/scripts/jumpmarks_or_scratchpad.sh
+++ i3/scripts/jumpmarks_or_scratchpad.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import i3ipc
 from sys import argv
+from time import sleep
 
 # Create the Connection object that can be used to send commands and subscribe
 # to events.
@@ -11,25 +12,26 @@ def focus(i3, mark):
     tree = i3.get_tree()
 
     try:
-    # If no marks found (e.g. if in an empty workspace), continue
         focused = tree.find_focused()
 
-        # If the mark is the same as focused container's, then move to scratchpad
+        # Case 1: Mark is the same as focused container
         if focused.marks[0] == mark:
             i3.command('[con_mark="^' + mark + '$"] move scratchpad')
             i3.command('[con_mark="^' + mark + '$"] floating toggle')
-        else:
-            i3.command('[con_mark="^' + mark + '$"] focus')
     except Exception:
         pass
 
-    # Since not currently focused, focus on mark
-    i3.command('[con_mark="^' + mark + '$"] focus')
 
-    if tree.find_marked(mark)[0].workspace().name == '__i3_scratch':
+    # Case 2: In Scratchpad
+    if tree.find_marked("^"+mark+"$")[0].workspace().name == '__i3_scratch':
+        # Since not currently focused, focus on mark
         # If mark was in scratchpad, unfloat it
+        i3.command('[con_mark="^' + mark + '$"] focus')
         i3.command('[con_mark="^' + mark + '$"] floating toggle')
         i3.command('[con_mark="^' + mark + '$"] floating toggle')
+    # Case 3: Not focused, not in Scratchpad
+    else:
+        i3.command('[con_mark="^' + mark + '$"] focus')
 
 focus(i3, mark)
 exit(0)
