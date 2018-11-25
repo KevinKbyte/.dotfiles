15f553206e206ad7d3f9eefbf6503deea5b95422
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2018-11-24 21:46:47 -0500

    changed kill memory hog script to kill cpu hog
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- i3/scripts/kill_cpu_hog.sh
+++ i3/scripts/kill_cpu_hog.sh
@@ -3,8 +3,9 @@
 # https://unix.stackexchange.com/questions/13968/show-top-five-cpu-consuming-processes-with-ps
 most_cpu_intensive_process=$(ps aux | sort -nrk 3,3 | head -n 1)
 cpu_usage=$(echo $most_cpu_intensive_process | awk '{print $3}')
-benchmark=50
+benchmark=75
 
+echo $cpu_usage
 if [[ $cpu_usage > $benchmark ]]; then
     kill $(echo $most_cpu_intensive_process | awk '{print $2}')
 fi
