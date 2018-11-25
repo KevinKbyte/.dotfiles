6dc5ac49dcad36214cb080d7758014ba3f05d335
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2018-11-24 19:11:40 -0500

    Added alt-tab functionality to also work when in an empty workspace
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- i3/scripts/alt_tab.sh
+++ i3/scripts/alt_tab.sh
@@ -2,6 +2,7 @@
 
 import i3ipc
 from sys import argv
+from time import sleep
 
 i3 = i3ipc.Connection()
 
@@ -17,7 +18,26 @@ def alt_tab(i3):
 
     focused = tree.find_focused()
 
-    if argv[1] == "next":
+    # if no class, then not focused on a container
+    # Then focus on the first node in the workspace
+    if not focused.window_class:
+        if argv[1] == 'next':
+            i3.command('workspace next')
+            current_workspace = i3.get_tree().find_focused().workspace()
+
+            first_container = current_workspace.descendents()[0]
+            first_container.command('focus')
+        else:
+            i3.command('workspace prev')
+            current_workspace = i3.get_tree().find_focused().workspace()
+
+            last_container = current_workspace.descendents()[-1]
+            print(first_container.name)
+            last_container.command('focus')
+
+        return
+
+    if argv[1] == 'next':
         i_x = (leaves.index(focused) + 1) % len(leaves)
     else:
         i_x = (leaves.index(focused) - 1) % len(leaves)
