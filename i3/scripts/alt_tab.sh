d62256764db46a927d6b31c1ac437fdd0775db58
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2018-12-26 00:04:24 -0500

    modified alt-tab to work in most likely all normal situations
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- i3/scripts/alt_tab.sh
+++ i3/scripts/alt_tab.sh
@@ -8,46 +8,48 @@ import itertools
 
 i3 = i3ipc.Connection()
 
-start_time = time.time()
+# start_time = time.time()
 
 def alt_tab(i3):
-    tree = i3.get_tree()
-    root = tree.root()
+    # # https://stackoverflow.com/questions/716477/join-list-of-lists-in-python
+    # leaves = list(itertools.chain.from_iterable([[ descendent for descendent in workspace.descendents() if descendent.name ] for workspace in tree.workspaces()]))
 
-    # https://stackoverflow.com/questions/716477/join-list-of-lists-in-python
-    leaves = list(itertools.chain.from_iterable([[ descendent for descendent in workspace.descendents() if descendent.name ] for workspace in tree.workspaces()]))
+    # # For some reason there is a None container if containers in scratchpad, so ignore it
+    # [leaves.remove(container) for container in root.scratchpad() if container.name in leaves]
 
-    # For some reason there is a None container if containers in scratchpad, so ignore it
-    [leaves.remove(container) for container in root.scratchpad() if container.name in leaves]
-
-    focused = tree.find_focused()
+    current_workspace = i3.get_tree().find_focused().workspace()
+    focused = current_workspace.find_focused()
+    leaves = current_workspace.leaves()
 
     # if no class, then not focused on a container
     # Then focus on the first node in the workspace
-    if not focused.window_class:
+    try:
+        if argv[1] == 'next':
+            i_x = leaves.index(focused) + 1
+        else:
+            i_x = leaves.index(focused) - 1
+    except ValueError: # if focused == None, NoneType obj not in list leaves
+        pass
+
+    if not focused or len(leaves) == i_x or i_x < 0:
         if argv[1] == 'next':
             i3.command('workspace next')
             current_workspace = i3.get_tree().find_focused().workspace()
 
-            first_container = current_workspace.descendents()[0]
+            first_container = current_workspace.leaves()[0]
             first_container.command('focus')
         else:
             i3.command('workspace prev')
             current_workspace = i3.get_tree().find_focused().workspace()
 
-            last_container = current_workspace.descendents()[-1]
+            last_container = current_workspace.leaves()[-1]
             last_container.command('focus')
 
         return
-
-    if argv[1] == 'next':
-        i_x = (leaves.index(focused) + 1) % len(leaves)
-    else:
-        i_x = (leaves.index(focused) - 1) % len(leaves)
 
     leaves[i_x].command('focus')
 
 alt_tab(i3)
 
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
 exit(0)
